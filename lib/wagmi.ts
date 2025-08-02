c4f00c5b1dc04656000633397d20b14744f44e1e
FjrREPO <jossfajar21@gmail.com>
2025-08-02 15:40:13 +0700

    feat(wallets): organize wallet imports and configuration for clarity

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -3,6 +3,16 @@ import "@/lib/polyfills";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { http } from "wagmi";
 import { etherlinkTestnet } from "wagmi/chains";
+import {
+  braveWallet,
+  coinbaseWallet,
+  metaMaskWallet,
+  okxWallet,
+  rabbyWallet,
+  rainbowWallet,
+  trustWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
 
 import { siteConfig } from "@/config/site";
 
@@ -10,6 +20,16 @@ const config = getDefaultConfig({
   appName: siteConfig.name,
   projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID || "",
   chains: [etherlinkTestnet],
+  wallets: [
+    {
+      groupName: "Recommended",
+      wallets: [metaMaskWallet, okxWallet, rabbyWallet, walletConnectWallet],
+    },
+    {
+      groupName: "Others",
+      wallets: [rainbowWallet, coinbaseWallet, braveWallet, trustWallet],
+    },
+  ],
   transports: {
     [etherlinkTestnet.id]: http("https://node.ghostnet.etherlink.com"),
   },
